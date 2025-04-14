caf1b8321fc3dad530f51fd8a2188da9473fdab2
sidalimer <merahisidali@gmail.com>
2025-04-14 15:22:33 +0200

    Initial commit

---
+++ old_app.ts
@@ -0,0 +1,59 @@
+type Role = "admin" | "user" | "guest";
+
+interface Product {
+  readonly id: number;
+  name: string;
+  price?: number;
+}
+
+interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+  role: Role;
+  isActive: boolean;
+  createdAt?: Date;
+  products: Product[];
+}
+
+const user: User = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  isActive: true,
+  role: "admin",
+  createdAt: new Date(),
+  products: [
+    {
+      id: 1,
+      name: "Product 1",
+      price: 20,
+    },
+    {
+      id: 1,
+      name: "Product 1",
+    },
+  ],
+};
+
+const fullName = (user: User): string => {
+  return `${user.firstName} ${user.lastName}`;
+};
+
+const fullNameBis = (fistName: string, lastName: string): void => {
+  console.log(`${fistName} ${lastName}`);
+};
+fullNameBis(user.firstName, user.lastName);
+fullName(user);
+//interface IProps {
+// lastName: string;
+//firstName: string;
+//}
+
+// const Index = ({lastName, firstName}:IProps) => {
+//     return (
+//         <div>
+//         <p>{ lastName } {firstName} </p>
+//         </div>
+//     )
+// }
